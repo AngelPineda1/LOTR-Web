a06cfea0af85f9df8d85daf7c312e54453c9e37a
AngelPineda1 <201G0276@rcarbonifera.tecnm.mx>
2023-12-02 02:05:55 -0600

    se crearon las acciones post de publicaciones

--- LOTR-Web/Areas/Admin/Controllers/PublicacionesController.cs
+++ LOTR-Web/Areas/Admin/Controllers/PublicacionesController.cs
@@ -2,6 +2,7 @@
 
 namespace LOTR_Web.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class PublicacionesController : Controller
     {
         public IActionResult Index()
@@ -12,10 +13,27 @@ namespace LOTR_Web.Areas.Admin.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+        public IActionResult Editar(int id)
+        {
+            return View();
+
+        }
+
+        [HttpPost]
         public IActionResult Editar()
         {
             return View();
         }
+        public IActionResult Eliminar(int id)
+        {
+            return View();
+        }
+        [HttpPost]
         public IActionResult Eliminar()
         {
             return View();
