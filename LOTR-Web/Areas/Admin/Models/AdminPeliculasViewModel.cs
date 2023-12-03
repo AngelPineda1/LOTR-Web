fceac3ac0a1ee543da5fd04b4caf706542dd4276
AngelPineda1 <201G0276@rcarbonifera.tecnm.mx>
2023-12-02 18:39:35 -0600

    creacion de vm de peliculas

---
+++ LOTR-Web/Areas/Admin/Models/AdminPeliculasViewModel.cs
@@ -0,0 +1,10 @@
+﻿using LOTR_Web.Models.Entities;
+
+namespace LOTR_Web.Areas.Admin.Models
+{
+    public class AdminPeliculasViewModel
+    {
+
+        public Peliculas Peliculas { get;set;}
+    }
+}
