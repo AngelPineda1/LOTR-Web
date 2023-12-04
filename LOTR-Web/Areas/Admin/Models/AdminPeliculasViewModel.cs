d02c392575ac987cd7c663196edaad2f3185c258
AngelPineda1 <201G0276@rcarbonifera.tecnm.mx>
2023-12-03 21:40:24 -0600

    Se termino el viewmodel para administrar peliculas

--- LOTR-Web/Areas/Admin/Models/AdminPeliculasViewModel.cs
+++ LOTR-Web/Areas/Admin/Models/AdminPeliculasViewModel.cs
@@ -5,6 +5,26 @@ namespace LOTR_Web.Areas.Admin.Models
     public class AdminPeliculasViewModel
     {
 
-        public Peliculas Peliculas { get;set;}
+        public PeliculasModel Peliculas { get; set; } = null!;
+        public IEnumerable<EstudiosModel> Estudios { get; set; } = null!;
+    }
+    public class EstudiosModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+    }
+    public class PeliculasModel
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; } = null!;
+
+        public DateTime FechaPublicacion { get; set; }
+
+        public string Descripcion { get; set; } = null!;
+
+        public int IdEstudio { get; set; }
+
+        public int IdUsuario { get; set; }
     }
 }
