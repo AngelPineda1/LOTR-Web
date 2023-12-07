bfad06c9248d35f27a23ff8c2d82f8192cff5c54
AngelPineda1 <201G0276@rcarbonifera.tecnm.mx>
2023-12-06 19:26:55 -0600

    se agrego el index de Juegos,Peliculas,Publicaciones, Libros se modificaron los layouts y se agregaron archos css para el diseno

---
+++ LOTR-Web/Areas/Admin/Controllers/LibrosController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LOTR_Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class LibrosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
